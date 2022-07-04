f956782df67167e28ce232edbc5fa2c498a1f7ce
Mandeep Dangar <mandeepdangar5138@gmail.com>
2022-07-04 21:23:01 +0530

    formatted nearHundread and solved posneg problem

--- JavaScript/Warmup-1/nearHundred.js
+++ JavaScript/Warmup-1/nearHundred.js
@@ -1,15 +1,14 @@
-function nearHundred(n){
-    const rem = n%100;
-    if((rem >= 90 || rem <= 10 ) && n > 10){
-      return true;
-    }
-    return false;
+function nearHundred(n) {
+  const rem = n % 100;
+  if ((rem >= 90 || rem <= 10) && n > 10) {
+    return true;
   }
+  return false;
+}
 
-  console.log(nearHundred(0));
-  console.log(nearHundred(110));
-  console.log(nearHundred(193));
-  console.log(nearHundred(95));
-  console.log(nearHundred(55));
-  console.log(nearHundred(145));
-  
+console.log(nearHundred(0));
+console.log(nearHundred(110));
+console.log(nearHundred(193));
+console.log(nearHundred(95));
+console.log(nearHundred(55));
+console.log(nearHundred(145));
---
+++ JavaScript/Warmup-1/posneg.js
@@ -0,0 +1,21 @@
+function posNeg(a, b, negative){
+    if((negative === true) && (a <= 0 && b <= 0))
+    {
+        return true;
+    }else if ((negative === true) && (a >= 0 || b >= 0))
+    {
+      return false;
+    }
+    if( a<= 0 && b <= 0)
+    {
+        return false;
+    }if(a<=0||b<=0){
+      return true;
+    }
+    return false;
+  }
+
+  console.log(posNeg(-1,-1,true));
+  console.log(posNeg(-1,-1,false));
+  console.log(posNeg(-1,1,true));
+  console.log(posNeg(-1,1,false));
